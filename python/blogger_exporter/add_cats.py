092008e73d161590ce84da57aed84eacccb405cc
Dan Aukes <danaukes@gmail.com>
2020-06-15 13:39:38 -0700

    first commit

---
+++ python/blogger_exporter/add_cats.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 20:01:10 2019
+
+@author: danaukes
+"""
+
+import yaml
+import os
+import sys
+import glob
+
+files = glob.glob('export/*.md')
+
+filenames = [os.path.splitext(item)[0] for item in files]
+filenames = [os.path.split(item)[1] for item in filenames]
+
+file_mapping = dict(zip(filenames,files))
+
+with open('categories.yaml') as f:
+    categories = yaml.load(f)
+with open('titles.yaml') as f:
+    titles = yaml.load(f)
+
+
+
+for key,value in categories.items():    
+    dict1 = {'tags':categories[key]}
+    dict2 = {'title':titles[key]}
+    with open(file_mapping[key],'r+') as f:
+#        f.seek(0)
+        content = f.read()
+        f.seek(0)
+        f.write('---\n'+yaml.dump(dict2)+yaml.dump(dict1)+'---\n'+content)
+        
---
+++ python/blogger_exporter/export_blog.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 13:01:08 2019
+
+@author: daukes
+"""
+import time
+import os
+import sys
+import requests
+import PIL
+import PIL.Image
+import shutil
+
+imtypemapping= {'jpeg':'jpg','png':'png'}
+
+def cleantext(text_in):
+    text_out = "".join([c for c in text_in if c.isalpha() or c.isdigit() or c==' ']).rstrip()
+    text_out=text_out.replace(' ','-')
+    text_out=text_out.lower()
+    return text_out
+
+
+def printattrs(item):
+    for item2 in dir(item):
+        print(getattr(item,item2))
+
+def get_children(root):
+    children = []
+    searchlist = [root]
+    
+    while not not searchlist:
+        item=searchlist.pop(0)
+        items = item.getchildren()
+        searchlist.extend(items)
+        children.extend(items)
+    return children
+
+def get_post_html(post):
+    item = post.find('{http://www.w3.org/2005/Atom}content')
+    text = item.text
+    textb = text.encode('utf-8')
+    root2 = et.HTML(textb)
+    return root2    
+    
+
+#filename = 'blog-09-05-2019.xml'
+#filename = 'blog-12-27-2019.xml'    
+filename = 'blog-12-30-2019.xml'    
+    
+with open(filename,'rb') as f:
+    output = f.readlines()
+
+output=b''.join(output)
+
+
+import lxml.etree as et
+import lxml.html as html
+
+root = et.XML(output)
+#c = root.getchildren()[0]
+
+children = get_children(root)    
+childtags = [item.tag for item in children]
+#print(childtags)
+#
+
+posts = []
+for ii,item in enumerate(children):
+    if 'entry' in item.tag:
+#        print(item.tag)
+        cat = item.find('{http://www.w3.org/2005/Atom}category')
+#        print(cat.attrib['term'])
+        if cat.attrib['term'] =='http://schemas.google.com/blogger/2008/kind#post':
+            posts.append(item)
+#        for item2 in item.getchildren():
+#            try:
+#                print('  '+item2.text)
+#            except TypeError:
+#                pass
+#        break
+        
+#        print(cat.text)
+#c=children[ii]    
+#c2 = c.find('{http://www.w3.org/2005/Atom}category')
+#
+#printattrs(c2)
+
+
+titles = []
+        
+#for post in children:
+#    for item in post:
+#        if 'title' in item.tag.lower():
+#            titles.append(item.text)
+
+#titles.sort()
+#print(titles)
+
+posts = []
+            
+for item in children:
+    
+    if 'category' in item.tag.lower():
+#        print(item.tag)
+        if 'term' in item.attrib:
+            if 'post' in item.attrib['term'].lower():
+                posts.append(item.getparent())
+#                print(item.getparent())
+                
+#print(entries)
+                
+#                title
+for post in posts:
+    title = None
+    for item in post:
+        if 'title' in item.tag.lower():
+            title = item.text
+    if title is None:
+        print(post)
+    else:
+        print(title)
+    titles.append(title)
+#        else:
+#            print(item)
+    
+images = []
+for post in posts:
+    root2 = get_post_html(post)
+    children2=get_children(root2)
+    my_images = []
+    for item in children2:
+        try:
+            tag = item.tag
+            if tag.lower()=='img':
+                my_images.append(item)
+        except AttributeError:
+            pass
+#    my_images = [item for item in children2 if (item.tag).lower()=='img']
+    images.extend(my_images)
+#print(images)
+
+srcs = [item.attrib['src'] for item in images]    
+srcs = list(set(srcs))
+mapping = []
+for ii,item in enumerate(srcs):
+    ftype = os.path.splitext(item)[1].lower()
+    mapping.append([item,'figure_{0:03.0f}{1:s}'.format(ii,ftype)])
+    
+#fnames = [os.path.split(item)[1] for item in srcs]
+#fnames.sort()
+
+
+if os.path.exists('export'):
+    shutil.rmtree('export')
+time.sleep(1)
+os.mkdir('export')
+os.mkdir('export/figures')
+
+for ii,(key,value) in enumerate(mapping):
+    r = requests.get(key) # create HTTP response object 
+    with open('export/figures/'+value,'wb') as f: 
+        f.write(r.content)     
+    img = PIL.Image.open('export/figures/'+value)
+    itype = imtypemapping[img.format.lower()]
+    del img
+    froot,ctype = os.path.splitext(value)
+    new = froot+'.'+itype.lower()
+    os.rename('export/figures/'+value,'export/figures/'+froot+'.'+itype.lower())
+    mapping[ii][1] = new
+
+mapping_fwd = dict([tuple(item) for item in mapping])
+    
+for post,title in zip(posts,titles):
+    item = post.find('{http://www.w3.org/2005/Atom}content')
+    text = item.text
+    textb = text.encode('utf-8')
+    root2 = et.HTML(textb)
+    children2=get_children(root2)
+    for item in children2:
+        try:
+            tag = item.tag
+            if tag.lower()=='img':
+                item.attrib['src']='figures/'+mapping_fwd[item.attrib['src']]
+        except AttributeError:
+            pass
+    textb2 = et.tostring(root2, pretty_print=True)
+    filename = cleantext(title)
+    with open('export/'+filename+'.html','wb') as f:
+#        f.writelines(textb)
+        f.write(textb2)
